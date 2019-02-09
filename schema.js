1539297414b2fe789e94fe1aac67d165fe1a7d43
hereiam123 <bdemaio1@gmail.com>
2019-02-08 21:28:51 -0500

    Setup for testing

--- schema.js
+++ schema.js
@@ -7,6 +7,13 @@ const {
   GraphQLNonNull
 } = require("graphql");
 
+//Test data
+const customers = [
+  { id: "1", name: "John Doe", email: "jdoe@gmail.com", age: 35 },
+  { id: "2", name: "Steve SMith", email: "steve@gmail.com", age: 25 },
+  { id: "3", name: "Jane Jonah", email: "jane@gmail.com", age: 46 }
+];
+
 //Customer Type
 const CustomerType = new GraphQLObjectType({
   name: "Customer",
@@ -21,9 +28,23 @@ const CustomerType = new GraphQLObjectType({
 //Root Query
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
-  customer: {
-    type: CustomerType
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id == args.id) {
+            return customers[i];
+          }
+        }
+      }
+    }
   }
 });
 
-module.exports = new GraphQLSchema({});
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
